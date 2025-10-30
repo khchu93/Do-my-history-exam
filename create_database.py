cd8f16059e94ea01c30f7797c5c4587a952a6650
Ka Hei Chu <90285104+khchu93@users.noreply.github.com>
2025-10-30 09:56:07 -0400

    test

--- create_database.py
+++ create_database.py
@@ -1,14 +1,29 @@
-# from langchain.document_loaders import DirectoryLoader
+import nltk
+
+# required_resources = [
+#     'punkt', 
+#     'punkt_tab', 
+#     'averaged_perceptron_tagger',
+#     'averaged_perceptron_tagger_eng',
+#     'stopwords',
+#     'wordnet',
+#     'omw-1.4'
+# ]
+
+# for res in required_resources:
+#     nltk.download(res)
+
 from langchain_community.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema import Document
-# from langchain.embeddings import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 import openai 
 from dotenv import load_dotenv
 import os
 import shutil
+
+from openai import OpenAI
 
 # Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
@@ -59,9 +74,17 @@ def save_to_chroma(chunks: list[Document]):
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
+
+    # client = OpenAI(api_key="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")
+    # response = client.chat.completions.create(
+    #     model="gpt-5-nano",
+    #     input="write a haiku about ai",
+    #     store=True,
+    # )
+
     # Create a new DB from the documents.
     db = Chroma.from_documents(
-        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+        chunks, OpenAIEmbeddings(model="text-embedding-3-small"), persist_directory=CHROMA_PATH
     )
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
